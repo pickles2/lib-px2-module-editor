dc610cc7739be6065fb7b8e89e39067133e22e90
Tomoya Koyanagi <tomk79@gmail.com>
2017-01-12 00:05:55 +0900

    (dist)

--- dist/pickles2-module-editor.js
+++ dist/pickles2-module-editor.js
@@ -16382,6 +16382,24 @@ module.exports = exports['default'];
 		}
 
 		/**
+		 * broccoli モジュールのコードの変更をすべて保存する
+		 */
+		this.saveModuleCode = function(moduleId, data, callback){
+			callback = callback || function(){};
+			this.gpiBridge(
+				{
+					'api':'saveModuleCode',
+					'moduleId': moduleId,
+					'data': data
+				},
+				function(result){
+					callback(result);
+				}
+			);
+			return;
+		}
+
+		/**
 		 * プログレスを表示する
 		 */
 		this.progress = function( callback ){
@@ -16494,9 +16512,15 @@ module.exports = function(px2me, $canvasContent, options, callback){
 		}); })
 		.then(function(){ return new Promise(function(rlv, rjt){
 			// 編集画面を描画
-			console.log(options);
+			// console.log(options);
 			px2me.getModuleCode( options.moduleId, function(getModuleCode){
-				console.log(getModuleCode);
+				// console.log(getModuleCode);
+
+				if( !getModuleCode.editable ){
+					alert('このモジュールは編集許可されていないパスにあります。');
+					rjt();
+					return;
+				}
 
 				var html = px2me.bindEjs(
 					px2me.getTemplates('editModule'),
@@ -16506,16 +16530,35 @@ module.exports = function(px2me, $canvasContent, options, callback){
 					}
 				);
 				$canvasContent.html('').append(html);
+
+				$canvasContent.find('[name=infoJson]').val( getModuleCode.infoJson );
+				$canvasContent.find('[name=template]').val( getModuleCode.template );
+				$canvasContent.find('[name=templateExt]').val( getModuleCode.templateExt );
+				$canvasContent.find('[name=css]').val( getModuleCode.css );
+				$canvasContent.find('[name=cssExt]').val( getModuleCode.cssExt );
+				$canvasContent.find('[name=js]').val( getModuleCode.js );
+				$canvasContent.find('[name=jsExt]').val( getModuleCode.jsExt );
 				rlv();
 			} );
 		}); })
 		.then(function(){ return new Promise(function(rlv, rjt){
 			// イベントをセット
 			$canvasContent.find('button.pickles2-module-editor__save').on('click', function(e){
-				alert('開発中です。');
-				px2me.closeModal(function(){
-					px2me.loadPage('list', {}, function(){});
-				});
+				var data = {};
+				data.infoJson = $canvasContent.find('[name=infoJson]').val();
+				data.template = $canvasContent.find('[name=template]').val();
+				data.templateExt = $canvasContent.find('[name=templateExt]').val();
+				data.css = $canvasContent.find('[name=css]').val();
+				data.cssExt = $canvasContent.find('[name=cssExt]').val();
+				data.js = $canvasContent.find('[name=js]').val();
+				data.jsExt = $canvasContent.find('[name=jsExt]').val();
+				// console.log('data =',data);
+
+				px2me.saveModuleCode(options.moduleId, data, function(result){
+					px2me.closeModal(function(){
+						px2me.loadPage('list', {}, function(){});
+					});
+				})
 			});
 			rlv();
 		}); })
@@ -16528,6 +16571,15 @@ module.exports = function(px2me, $canvasContent, options, callback){
 			callback();
 			rlv();
 		}); })
+		.catch(function(){
+			px2me.closeProgress(function(){
+				px2me.closeModal(function(){
+					px2me.loadPage('list', {}, function(){
+						callback();
+					});
+				});
+			});
+		})
 	;
 
 }
@@ -16581,7 +16633,7 @@ module.exports = function(px2me, $canvasContent, options, callback){
 						break;
 					case 'editModule':
 						px2me.loadPage('editModule', {'moduleId': target}, function(){
-							alert(act);
+							// alert(act);
 						});
 						break;
 					case 'addNewModule':
