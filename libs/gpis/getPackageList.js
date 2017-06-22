44cdad65bbe2b183a9bfb0028d881149849a102f
Tomoya Koyanagi <tomk79@gmail.com>
2017-06-23 03:27:04 +0900

    編集できないモジュールセットの編集系ボタンを非表示にした

--- libs/gpis/getPackageList.js
+++ libs/gpis/getPackageList.js
@@ -8,6 +8,9 @@ module.exports = function(px2me, data, callback){
 		// console.log(broccoli);
 		broccoli.getPackageList(function(packageList){
 			// console.log('getPackageList', packageList);
+			for(var idx in packageList){
+				packageList[idx].isEditable = px2me.isEditablePath(packageList[idx].realpath);
+			}
 			callback(packageList);
 		});
 	});
