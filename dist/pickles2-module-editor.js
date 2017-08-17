aca6c3b69b4ca4f2191888f3b4a07d8bab479175
Tomoya Koyanagi <tomk79@gmail.com>
2017-08-17 15:25:46 +0900

    (dist)

--- dist/pickles2-module-editor.js
+++ dist/pickles2-module-editor.js
@@ -17796,6 +17796,8 @@ module.exports = function(px2me, $canvasContent, options, callback){
 	var $ = require('jquery');
 	var utils79 = require('utils79');
 	var Promise = require('es6-promise').Promise;
+	var pluginPackages = [],
+		broccoliPackages = [];
 
 	new Promise(function(rlv){rlv();})
 		.then(function(){ return new Promise(function(rlv, rjt){
@@ -17805,10 +17807,40 @@ module.exports = function(px2me, $canvasContent, options, callback){
 			} );
 		}); })
 		.then(function(){ return new Promise(function(rlv, rjt){
+			// インポート元の一覧を取得 - プラグインパッケージより
+			px2me.gpiBridge(
+				{
+					'api':'getPluginPackage'
+				},
+				function(packages){
+					try {
+						pluginPackages = packages.package_list.broccoliModules;
+					} catch (e) {
+					}
+					// console.log(pluginPackages);
+					rlv();
+				}
+			);
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
+			// インポート元の一覧を取得 - broccoliより
+			px2me.getPackageList(function(packageList){
+				try {
+					broccoliPackages = packageList;
+				} catch (e) {
+				}
+				// console.log(broccoliPackages);
+				rlv();
+			});
+		}); })
+		.then(function(){ return new Promise(function(rlv, rjt){
 			// 編集画面を描画
 			var html = px2me.bindEjs(
 				px2me.getTemplates('addNewPackage'),
-				{}
+				{
+					'pluginPackages': pluginPackages,
+					'broccoliPackages': broccoliPackages
+				}
 			);
 			$canvasContent.html('').append(html);
 
@@ -17831,6 +17863,7 @@ module.exports = function(px2me, $canvasContent, options, callback){
 						var data = {};
 						data.packageId = $canvasContent.find('[name=packageId]').val();
 						data.packageName = $canvasContent.find('[name=packageName]').val();
+						data.importFrom = $canvasContent.find('[name=import_from]:checked').val();
 
 						px2me.addNewPackage(data, function(result){
 							px2me.loadPage('list', {}, function(){
