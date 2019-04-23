e3acdf8e55eecbaa59da773d2a6c9b61890c03ee
Tomoya Koyanagi <tomk79@gmail.com>
2019-04-23 14:05:16 +0900

    PHP版とNodeJS版でテンプレートファイルを共有するようにした。

--- libs/gpis/getTemplates.js
+++ libs/gpis/getTemplates.js
@@ -6,16 +6,16 @@ module.exports = function(px2me, data, callback){
 
 	var templates = {};
 
-	templates['list'] = require('fs').readFileSync(__dirname+'/../templates/list.html.ejs').toString();
-	templates['editPackage'] = require('fs').readFileSync(__dirname+'/../templates/editPackage.html.ejs').toString();
-	templates['editCategory'] = require('fs').readFileSync(__dirname+'/../templates/editCategory.html.ejs').toString();
-	templates['editModule'] = require('fs').readFileSync(__dirname+'/../templates/editModule.html.ejs').toString();
-	templates['addNewPackage'] = require('fs').readFileSync(__dirname+'/../templates/addNewPackage.html.ejs').toString();
-	templates['addNewCategory'] = require('fs').readFileSync(__dirname+'/../templates/addNewCategory.html.ejs').toString();
-	templates['addNewModule'] = require('fs').readFileSync(__dirname+'/../templates/addNewModule.html.ejs').toString();
-	templates['deletePackage'] = require('fs').readFileSync(__dirname+'/../templates/deletePackage.html.ejs').toString();
-	templates['deleteCategory'] = require('fs').readFileSync(__dirname+'/../templates/deleteCategory.html.ejs').toString();
-	templates['deleteModule'] = require('fs').readFileSync(__dirname+'/../templates/deleteModule.html.ejs').toString();
+	templates['list'] = require('fs').readFileSync(__dirname+'/../../templates/list.html.ejs').toString();
+	templates['editPackage'] = require('fs').readFileSync(__dirname+'/../../templates/editPackage.html.ejs').toString();
+	templates['editCategory'] = require('fs').readFileSync(__dirname+'/../../templates/editCategory.html.ejs').toString();
+	templates['editModule'] = require('fs').readFileSync(__dirname+'/../../templates/editModule.html.ejs').toString();
+	templates['addNewPackage'] = require('fs').readFileSync(__dirname+'/../../templates/addNewPackage.html.ejs').toString();
+	templates['addNewCategory'] = require('fs').readFileSync(__dirname+'/../../templates/addNewCategory.html.ejs').toString();
+	templates['addNewModule'] = require('fs').readFileSync(__dirname+'/../../templates/addNewModule.html.ejs').toString();
+	templates['deletePackage'] = require('fs').readFileSync(__dirname+'/../../templates/deletePackage.html.ejs').toString();
+	templates['deleteCategory'] = require('fs').readFileSync(__dirname+'/../../templates/deleteCategory.html.ejs').toString();
+	templates['deleteModule'] = require('fs').readFileSync(__dirname+'/../../templates/deleteModule.html.ejs').toString();
 
 	callback(templates);
 	return;
--- php/gpis/getTemplates.php
+++ php/gpis/getTemplates.php
@@ -5,16 +5,16 @@
 return function($px2me, $data){
 	$templates = array();
 
-	$templates['list'] = file_get_contents(__DIR__.'/../templates/list.html.ejs');
-	$templates['editPackage'] = file_get_contents(__DIR__.'/../templates/editPackage.html.ejs');
-	$templates['editCategory'] = file_get_contents(__DIR__.'/../templates/editCategory.html.ejs');
-	$templates['editModule'] = file_get_contents(__DIR__.'/../templates/editModule.html.ejs');
-	$templates['addNewPackage'] = file_get_contents(__DIR__.'/../templates/addNewPackage.html.ejs');
-	$templates['addNewCategory'] = file_get_contents(__DIR__.'/../templates/addNewCategory.html.ejs');
-	$templates['addNewModule'] = file_get_contents(__DIR__.'/../templates/addNewModule.html.ejs');
-	$templates['deletePackage'] = file_get_contents(__DIR__.'/../templates/deletePackage.html.ejs');
-	$templates['deleteCategory'] = file_get_contents(__DIR__.'/../templates/deleteCategory.html.ejs');
-	$templates['deleteModule'] = file_get_contents(__DIR__.'/../templates/deleteModule.html.ejs');
+	$templates['list'] = file_get_contents(__DIR__.'/../../templates/list.html.ejs');
+	$templates['editPackage'] = file_get_contents(__DIR__.'/../../templates/editPackage.html.ejs');
+	$templates['editCategory'] = file_get_contents(__DIR__.'/../../templates/editCategory.html.ejs');
+	$templates['editModule'] = file_get_contents(__DIR__.'/../../templates/editModule.html.ejs');
+	$templates['addNewPackage'] = file_get_contents(__DIR__.'/../../templates/addNewPackage.html.ejs');
+	$templates['addNewCategory'] = file_get_contents(__DIR__.'/../../templates/addNewCategory.html.ejs');
+	$templates['addNewModule'] = file_get_contents(__DIR__.'/../../templates/addNewModule.html.ejs');
+	$templates['deletePackage'] = file_get_contents(__DIR__.'/../../templates/deletePackage.html.ejs');
+	$templates['deleteCategory'] = file_get_contents(__DIR__.'/../../templates/deleteCategory.html.ejs');
+	$templates['deleteModule'] = file_get_contents(__DIR__.'/../../templates/deleteModule.html.ejs');
 
 	return $templates;
 };
